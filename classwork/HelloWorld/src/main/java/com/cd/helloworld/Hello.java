79bc81ad24ce21abc9e02fa8cd51f9ca152b6bdf
dennistonc <dennistonc@gmail.com>
2022-10-13 22:01:49 -0500

    Hello World in NetBeans

---
+++ classwork/HelloWorld/src/main/java/com/cd/helloworld/Hello.java
@@ -0,0 +1,23 @@
+/*
+ *@author Catherine D
+ *email:
+ *date: 10.13.22
+ *purpose: My first Hello World program in an IDE for Java
+ */
+package com.cd.helloworld;
+
+/*
+ * @author Catherine
+ */
+
+/*
+ *We use the class Hello to correspond with the file we are using.
+ *public static void main tells Java where the program itself starts.
+ *out.println tells Java to print out the text inside the parentheses.
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Catherine!");
+    }
+}
