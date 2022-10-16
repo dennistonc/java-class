c06a8f24621c07f7af9c3483fac70870bd0e839e
dennistonc <dennistonc@gmail.com>
2022-10-15 19:51:13 -0500

    Exercise 1 Completed

---
+++ classwork/PracticeProgramming/src/main/java/com/sg/foundations/basics/ABeginning.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.basics;
+
+/**
+ *
+ * @author Catherine
+ */
+public class ABeginning {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello from the Guild!");
+        System.out.println("Typing code is easier than I thought ...");
+        System.out.println("Typing is easy... but using brainpower...?");
+        System.out.println("After I finish typing,");
+        System.out.println("I'll compile my code.");
+        System.out.println("And then when I run it,");
+        System.out.println("The console will print out all my brilliant words!");
+        System.out.println("And it all starts with 'Hello World!' ..");
+    }
+}
+
+/*
+Like a modern Gutenberg, the System.out.println() method takes in information
+and prints it to the console terminal easily. We can print out whole books,
+or even libraries, — this way.
+
+Don't forget that there are other printing methods too. System.out.print()
+for example, takes in information but doesn't also add in a new line character.
+*/
